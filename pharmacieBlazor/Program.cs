745ed93a31f89d0925149cbb09fe63437d750336
Amina Maatoug <aminamaatoug2002@gmail.com>
2025-12-17 16:32:41 +0100

    jdid y reba7

--- pharmacieBlazor/Program.cs
+++ pharmacieBlazor/Program.cs
@@ -1,9 +1,9 @@
-﻿using Front.Services;
-using Microsoft.AspNetCore.Components.Authorization;
+﻿using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using pharmacieBlazor;
 using pharmacieBlazor.Services;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
